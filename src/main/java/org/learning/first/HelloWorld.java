312c64c2e4524b3d8e178f6d85ea42878a943985
V-Satwik-Reddy <satwikstudies05@gmail.com>
2025-05-14 23:53:13 +0530

    ejvb

--- src/main/java/org/learning/first/HelloWorld.java
+++ src/main/java/org/learning/first/HelloWorld.java
@@ -3,6 +3,9 @@ package org.learning.first;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.learning.first.Model.*;
+
+import java.util.List;
 
 @RestController
 public class HelloWorld {
@@ -11,7 +14,9 @@ public class HelloWorld {
         return "Hello World";
     }
     @PostMapping("/helloyouname")
-    public static String helloUser(){
-        return "enter you name";
+    public Message welcomeMessage() {
+        Message m = new Message();
+        m.setMessage("Welcome to HelloWorld!");
+        return m;
     }
 }
