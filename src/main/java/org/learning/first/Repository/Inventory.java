dad4a36b4085752c7454a381101ff49b66210825
V-Satwik-Reddy <satwikstudies05@gmail.com>
2025-05-17 23:18:14 +0530

    added repo

---
+++ src/main/java/org/learning/first/Repository/Inventory.java
@@ -0,0 +1,9 @@
+package org.learning.first.Repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface Inventory extends MongoRepository<Inventory, String> {
+
+
+}
+
