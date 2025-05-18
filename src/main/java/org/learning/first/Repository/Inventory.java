3ed7f2e79df1cfc48cd1dfcc96edec5ac99ba987
V-Satwik-Reddy <satwikstudies05@gmail.com>
2025-05-18 22:43:52 +0530

    woops in naming the repo

--- src/main/java/org/learning/first/Repository/Inventory.java
+++
@@ -1,8 +0,0 @@
-package org.learning.first.Repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface Inventory extends MongoRepository<Inventory, String> {
-
-}
-
---
+++ src/main/java/org/learning/first/Repository/InventoryRepository.java
@@ -0,0 +1,12 @@
+package org.learning.first.Repository;
+
+import org.learning.first.Model.Inventory;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface InventoryRepository extends MongoRepository<Inventory, String> {
+
+
+}
+
