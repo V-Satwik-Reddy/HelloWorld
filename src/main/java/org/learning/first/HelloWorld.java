74dd434e96f196e98243dc9955e251fc07ba7c17
V-Satwik-Reddy <satwikstudies05@gmail.com>
2025-05-20 22:49:39 +0530

    s

--- src/main/java/org/learning/first/HelloWorld.java
+++ src/main/java/org/learning/first/HelloWorld.java
@@ -1,10 +1,7 @@
 package org.learning.first;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.learning.first.Model.*;
 
 import java.util.List;
@@ -16,5 +13,10 @@ public class HelloWorld {
         Message m = new Message();
         m.setMessage("HelloWorld!");
         return m;
+    }
+    @PostMapping("/addItem")
+    public Message addItem(@RequestBody) {
+        Message m = new Message();
+
     }
 }
