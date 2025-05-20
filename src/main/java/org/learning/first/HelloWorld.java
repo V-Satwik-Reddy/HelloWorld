c502302c4b60d7ddfd40243243d5b3ec9bfd8df8
V-Satwik-Reddy <satwikstudies05@gmail.com>
2025-05-20 22:50:48 +0530

    s

--- src/main/java/org/learning/first/HelloWorld.java
+++ src/main/java/org/learning/first/HelloWorld.java
@@ -1,5 +1,6 @@
 package org.learning.first;
 
+import org.learning.first.Repository.InventoryRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.learning.first.Model.*;
@@ -8,6 +9,8 @@ import java.util.List;
 
 @RestController
 public class HelloWorld {
+
+    InventoryRepository iR;
     @GetMapping("/")
     public Message helloWorld() {
         Message m = new Message();
@@ -15,8 +18,10 @@ public class HelloWorld {
         return m;
     }
     @PostMapping("/addItem")
-    public Message addItem(@RequestBody) {
+    public Message addItem(@RequestBody Inventory inventory ) {
         Message m = new Message();
-
+        iR.save(inventory);
+        m.setMessage("Item added successfully!");
+        return m;
     }
 }
